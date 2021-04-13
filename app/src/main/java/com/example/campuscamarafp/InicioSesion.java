365858d9471d21edd569766b05ac9ad45bb7db3d
tic <tic>
2021-04-13 11:05:15 +0200

    CampusCamaraFP

--- app/src/main/java/com/example/campuscamarafp/InicioSesion.java
+++ app/src/main/java/com/example/campuscamarafp/InicioSesion.java
@@ -3,24 +3,28 @@ package com.example.campuscamarafp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class InicioSesion extends AppCompatActivity {
 
+    private EditText et1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_inicio);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        et1 = (EditText)findViewById(R.id.etUsuario);
     }
 
     public void Registrar (View view){
         Intent i = new Intent(this, Registrarse.class);
+        i.putExtra("dato", et1.getText().toString());
         startActivity(i);
         Toast.makeText(this,"Resgistro",Toast.LENGTH_SHORT).show();
     }
