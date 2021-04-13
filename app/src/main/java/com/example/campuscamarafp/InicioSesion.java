ceba8d54509e8600e830337f02210b0dabe86c5e
tic <tic>
2021-04-13 10:35:55 +0200

    CampusCamaraFP

--- app/src/main/java/com/example/campuscamarafp/InicioSesion.java
+++ app/src/main/java/com/example/campuscamarafp/InicioSesion.java
@@ -2,12 +2,15 @@ package com.example.campuscamarafp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class InicioSesion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +18,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
+
+    public void Registrar (View view){
+        Intent i = new Intent(this, Registrarse.class);
+        startActivity(i);
+        Toast.makeText(this,"Resgistro",Toast.LENGTH_SHORT).show();
+    }
+
 }
---
+++ app/src/main/java/com/example/campuscamarafp/Registrarse.java
@@ -0,0 +1,14 @@
+package com.example.campuscamarafp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Registrarse extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_registro);
+    }
+}
