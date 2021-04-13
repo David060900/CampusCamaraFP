1606cfa734146fe6f394f663219b482f4b6026a3
tic <tic>
2021-04-13 11:57:05 +0200

    CampusCamaraFP

--- app/src/main/java/com/example/campuscamarafp/Registrarse.java
+++ app/src/main/java/com/example/campuscamarafp/Registrarse.java
@@ -1,16 +1,24 @@
 package com.example.campuscamarafp;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-
-
     public class Registrarse extends AppCompatActivity {
+
+        private Spinner spinner1;
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main_registro);
+
+            spinner1 = (Spinner)findViewById(R.id.spinnerCursos);
+
+            String cursos [] = {"DAM", "Marketing y Publicidad", "Comercio Internacional"};
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_cursos, cursos);
+            spinner1.setAdapter(adapter);
         }
     }
