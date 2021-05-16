16276d0175d8d21f2f301c47647732003289753c
David060900 <davidzpr4@gmail.com>
2021-05-16 23:55:36 +0200

    d

--- app/src/main/java/com/example/campuscamarafp/AdapterListaAlumnos.java
+++ app/src/main/java/com/example/campuscamarafp/AdapterListaAlumnos.java
@@ -6,18 +6,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.campuscamarafp.entidades.Alumno;
+
+import java.util.ArrayList;
 
 public class AdapterListaAlumnos extends BaseAdapter {
 
-//https://unprogramador.com/hacer-una-lista-en-android-studio/
+    private Context context;
+    private ArrayList<Alumno> listItems;
+
+    public AdapterListaAlumnos(Context context, ArrayList<Alumno> listItems) {
+        this.context = context;
+        this.listItems = listItems;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return listItems.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return listItems.get(position);
     }
 
     @Override
@@ -27,6 +39,13 @@ public class AdapterListaAlumnos extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        if(convertView == null){
+            LayoutInflater inf = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inf.inflate(R.layout.lista, null);
+        }
+        TextView nombre, apellidos;
         return null;
+
     }
 }
