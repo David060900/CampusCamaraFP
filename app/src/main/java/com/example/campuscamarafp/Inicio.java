9b6a87819bfe3ad67cecfad495da3afe68b9ebf3
tic <tic>
2021-05-24 12:45:36 +0200

    24 -5

--- app/src/main/java/com/example/campuscamarafp/Inicio.java
+++ app/src/main/java/com/example/campuscamarafp/Inicio.java
@@ -62,8 +62,8 @@ public class Inicio  extends AppCompatActivity {
         btn.setOnClickListener(v -> {
             Toast.makeText(Inicio.this, "Reservando disponibilidad" , Toast.LENGTH_SHORT).show();
             bd.execSQL("delete from impartir where correo_alumnos = '" + impartir.getCorreo_alumnos() + "';");
+            finish();
         });
-        finish();
     }
 
     public ArrayList lvBanco(){
